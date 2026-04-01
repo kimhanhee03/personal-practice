2620745424d6e788880f3cb4656c87405f5c5574
hanhe <hanhe@DESKTOP-ESHD740>
2026-04-01 21:06:53 +0900

    study

---
+++ Practice/src/test.java
@@ -0,0 +1,34 @@
+class A
+{
+	int aa=100;
+}
+class B
+{
+	public void setAA(A a)
+	{
+		a.aa=1000;
+	}
+	public void setStr(String s)
+	{
+		s="홍길동";
+	}
+}
+
+public class test {
+
+	public static void main(String[] args)  throws Exception{
+		// TODO Auto-generated method stub
+		
+		 A a=new A();
+	        System.out.println(a.aa);
+	        B b=new B();
+	        b.setAA(a);
+	        System.out.println(a.aa);
+	        
+	        String name="심청이";
+	        System.out.println(name);
+	        b.setStr(name);
+	        System.out.println(name);
+	}
+
+}
